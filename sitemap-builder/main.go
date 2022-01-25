14b7dc12f47fa4addca73c9d53cb25e4d84096bc
Robson Silva <robsantossilva@gmail.com>
2022-01-25 15:16:26 -0300

    get webpage, parsing and building Links

--- sitemap-builder/main.go
+++ sitemap-builder/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
+
+	link "github.com/gophercises/link/students/ccallergard"
 )
 
 func main() {
+
+	/**
+	1. Get the webpage
+	2. parse all the links on the page
+	3. build proper urls with our links
+	4. filter out any links w/ a diff domain
+	5. Find all pages (BFS)
+	*/
+
 	urlFlag := flag.String("url", "https://gophercises.com", "the url that you want to build a sitemap for")
 	flag.Parse()
 
@@ -16,10 +30,25 @@ func main() {
 
 	defer res.Body.Close()
 
-	//io.Copy(os.Stdout, res.Body)
+	reqUrl := res.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
 
-	//body, err := ioutil.ReadAll(res.Body)
-	//fmt.Println(body)
+	links, _ := link.Parse(res.Body)
+	var hrefs []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			hrefs = append(hrefs, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			hrefs = append(hrefs, l.Href)
+		}
+	}
 
-	//links, _ := link.
+	for _, href := range hrefs {
+		fmt.Println(href)
+	}
 }
