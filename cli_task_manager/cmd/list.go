de6b0a9d8cad355d9cd55f0ee2beb2e8d6758c0e
Robson Silva <robsantossilva@gmail.com>
2022-02-19 20:37:50 -0300

    cli task manager

---
+++ cli_task_manager/cmd/list.go
@@ -0,0 +1,24 @@
+/*
+Copyright © 2022 Robson Silva <robsantossilva@gmail.com>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all of your tasks",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("list called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(listCmd)
+}
---
+++ cli_task_manager/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/robsantossilva/gophercises-cli_task_manager/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}
